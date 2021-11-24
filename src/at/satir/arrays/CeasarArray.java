68bbdfb6c9ef5d9a1b2594f0e49555d52fa5648b
serkan <serkan.satirx61@gmail.com>
2021-11-24 08:51:51 +0100

    test

--- src/at/satir/arrays/CeasarArray.java
+++ src/at/satir/arrays/CeasarArray.java
@@ -2,48 +2,49 @@ package at.satir.arrays;
 
 public class CeasarArray {
 
-//    public static void main(String[] args) {
-//        String test = "Ich bin ein sicherer Satz!";
-//        int key = 2;
-//
-//
-//        String ciphertext = encrypt(test, key);
-//        System.out.println(test + " -> " + ciphertext);
-//
-//        String decoded = decrypt(ciphertext, key);
-//        System.out.println(ciphertext + " -> " + decoded);
-//
-//    }
-//
-//    public static String encrypt(String test, int key) {
-//        String ciphertext = "";
-//        test = test.toLowerCase();
-//        for (int i = 0; i < test.length(); i++) {
-//            char next = test.charAt(i);
-//            ciphertext = ciphertext + shift(next, key);
-//        }
-//        return ciphertext;
-//    }
-//
-//    public static char shift(char letter, int shift) {
-//
-//        if (letter >= 'a' && letter <= 'z') {
-//            letter += shift;
-//            while (letter > 'z') {
-//                letter -= 26;
-//            }
-//        }
-//        return letter;
-//    }
-//
-//    public static String decrypt(String ciphertext, int key) {
-//        String decoded = "";
-//        ciphertext = ciphertext.toLowerCase();
-//        for (int i = 0; i < ciphertext.length(); i++) {
-//            char next = ciphertext.charAt(i);
-//            decoded = decoded + shift(next, 26 - key);
-//        }
-//        return decoded;
-//    }
+    public static void main(String[] args) {
+        String test = "Dies ist ein Satz!";
+        int key = 2;
+
+
+        String ciphertext = encrypt(test, key);
+        System.out.println(test + " -> " + ciphertext);
+
+        String decoded = decrypt(ciphertext, key);
+        System.out.println(ciphertext + " -> " + decoded);
+    }
+
+    public static char shift(char letter, int shift){
+        if(letter>='a' && letter <='z'){
+            letter += shift;
+            while(letter > 'z'){
+                letter -= 26;
+            }
+        }
+        return letter;
+    }
+
+    public static String encrypt(String test, int key) {
+        String ciphertext = "";
+        test = test.toLowerCase();
+
+        for (int i = 0; i < test.length(); i++) {
+            char next = test.charAt(i);
+            ciphertext = ciphertext + shift(next, key);
+        }
+        return ciphertext;
+    }
+
+
+    public static String decrypt(String ciphertext, int key) {
+        String decoded = "";
+        ciphertext = ciphertext.toLowerCase();
+
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char next = ciphertext.charAt(i);
+            decoded = decoded + shift(next, 26 - key);
+        }
+        return decoded;
+    }
 
 }
