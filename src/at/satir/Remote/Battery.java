45941cb4c55a6e41d76bc775077f3a4e1a209a8d
serkan <serkan.satirx61@gmail.com>
2022-02-04 11:20:36 +0100

    turnON() and turnOFF() Method created

--- src/at/satir/Remote/Battery.java
+++ src/at/satir/Remote/Battery.java
@@ -2,12 +2,42 @@ package at.satir.Remote;
 
 public class Battery {
     private boolean chargingStatus;
-    private int batteryStatus;
+    private double batteryStatus;
+    private String batteryName;
 
-    public Battery(boolean chargingStatus, int batteryStatus) {
+    public Battery(boolean chargingStatus, double batteryStatus, String batteryName) {
         this.chargingStatus = chargingStatus;
         this.batteryStatus = batteryStatus;
+        this.batteryName = batteryName;
     }
+
+
+
+    public void turnOn() {
+        if (this.chargingStatus = false) {
+            this.chargingStatus = true;
+            this.batteryStatus = this.batteryStatus - (this.batteryStatus / 100 * 0.95);
+        } else {
+            System.out.println("Mein Name ist " + batteryName + " Mein Verbraucher ist angeschlossen");
+        }
+    }
+
+    public void turnOff() {
+        if (this.chargingStatus = true) {
+            this.chargingStatus = false;
+            System.out.println("Mein Name ist " + batteryName + " Kein Verbraucher angeschlossen");
+        }
+    }
+
+
+    public boolean getChargingStatus() {
+        return chargingStatus;
+    }
+
+    public double getBatteryStatus() {
+        return batteryStatus;
+    }
+
 
 
 
