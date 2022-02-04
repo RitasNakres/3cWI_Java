0108428f881f9400c343d0306ca26c5a59904d20
serkan <serkan.satirx61@gmail.com>
2022-02-04 11:37:47 +0100

    turnOn() and turnOff() Method works

--- src/at/satir/Remote/Main.java
+++ src/at/satir/Remote/Main.java
@@ -1,7 +1,16 @@
 package at.satir.Remote;
 
+import at.satir.objects.Producer;
+
 public class Main {
     public static void main(String[] args) {
 
+        Battery b1 = new Battery(true,150,"BatteryOne");
+        Battery b2 = new Battery(false,89,"BatteryTwo");
+
+        b1.turnOff();
+        b2.turnOn();
+        b1.getBatteryStatus();
+        b2.getBatteryStatus();
     }
 }
