0fc386e5926d55df1f1865cd27f8754f6c2cff59
serkan <serkan.satirx61@gmail.com>
2022-02-25 11:19:53 +0100

    Added batteries list

--- src/at/satir/Remote/Remote.java
+++ src/at/satir/Remote/Remote.java
@@ -1,4 +1,16 @@
 package at.satir.Remote;
+import java.util.List;
 
 public class Remote {
+    private Battery battery;
+    private List<Battery> batteries;
+
+    public Remote(Battery battery) {
+        this.battery = battery;
+    }
+
+    public void getAllBatteryStatus() {double getAllBatteryStatus;
+        getAllBatteryStatus = (this.battery.getBatteryStatus() + this.battery.getBatteryStatus()) / 2;
+    }
+
 }
