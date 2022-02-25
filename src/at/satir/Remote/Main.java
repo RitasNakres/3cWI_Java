745b1b70c8048524cc65e44b090662a91f9ed8f9
serkan <serkan.satirx61@gmail.com>
2022-02-25 11:50:03 +0100

    Trying to get the BatteryStatus

--- src/at/satir/Remote/Main.java
+++ src/at/satir/Remote/Main.java
@@ -1,16 +1,18 @@
 package at.satir.Remote;
 
-import at.satir.objects.Producer;
-
 public class Main {
     public static void main(String[] args) {
 
-        Battery b1 = new Battery(true,150,"BatteryOne");
-        Battery b2 = new Battery(false,89,"BatteryTwo");
+        Battery b1 = new Battery(100, false);
+        Battery b2 = new Battery(80, true);
+        Remote r1 = new Remote(b1);
+        r1.addBattery(b2);
+        b1.setBatteryStatus();
+
 
         b1.turnOff();
         b2.turnOn();
         b1.getBatteryStatus();
-        b2.getBatteryStatus();
+        r1.getAllBatteryStatus();
     }
 }
