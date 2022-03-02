774cbf9457a47cf0f29718aae56685a383f22c44
serkan <serkan.satirx61@gmail.com>
2022-03-02 09:36:04 +0100

    reprogrammed turnOn and turnOff Methods

--- src/at/satir/Remote/Remote.java
+++ src/at/satir/Remote/Remote.java
@@ -1,16 +1,37 @@
 package at.satir.Remote;
+
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Remote {
-    private Battery battery;
-    private List<Battery> batteries;
+    private Battery battery1;
+    private Battery battery2;
 
-    public Remote(Battery battery) {
-        this.battery = battery;
+
+    public Remote(Battery battery1, Battery battery2) {
+        this.battery1 = battery1;
+        this.battery2 = battery2;
     }
 
-    public void getAllBatteryStatus() {double getAllBatteryStatus;
-        getAllBatteryStatus = (this.battery.getBatteryStatus() + this.battery.getBatteryStatus()) / 2;
+    public void turnOn() {
+        double newBatteryStatus1 = this.battery1.getBatteryStatus() - 5;
+        this.battery1.setBatteryStatus(newBatteryStatus1);
+        double newBatteryStatus2 = this.battery2.getBatteryStatus() -5;
+        this.battery2.setBatteryStatus(newBatteryStatus2);
     }
+
+    public void turnOff() {
+        System.out.println("Kein Verbraucher angeschlossen");
+        System.out.println("Kein Verbraucher angeschlossen");
+    }
+
+
+    public void getAllBatteryStatus() {
+        double getAllBatteryStatus = (this.battery1.getBatteryStatus() + this.battery2.getBatteryStatus()) / 2;
+    }
+
+
+
 
 }
