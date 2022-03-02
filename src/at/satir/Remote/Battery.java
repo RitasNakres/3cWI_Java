48c92249382cc3d35c63b13b343fd78613731890
serkan <serkan.satirx61@gmail.com>
2022-03-02 08:55:49 +0100

    getter and setter renewed

--- src/at/satir/Remote/Battery.java
+++ src/at/satir/Remote/Battery.java
@@ -1,40 +1,55 @@
 package at.satir.Remote;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Battery {
     private boolean chargingStatus;
     private double batteryStatus;
-    private String batteryName;
+    private List<Battery> batteries;
 
-    public Battery(boolean chargingStatus, double batteryStatus, String batteryName) {
-        this.chargingStatus = chargingStatus;
+    public Battery(double batteryStatus, boolean chargingStatus) {
         this.batteryStatus = batteryStatus;
-        this.batteryName = batteryName;
+        this.chargingStatus = chargingStatus;
+        this.batteries = new ArrayList<>();
     }
 
-
-    public void turnOn() {
-        if (this.chargingStatus = false) {
-            this.chargingStatus = true;
-            this.batteryStatus = this.batteryStatus - (this.batteryStatus / 100 * 0.95);
-        } else {
-            System.out.println("Mein Name ist " + batteryName + " Mein Verbraucher ist angeschlossen");
-        }
+    public void addBattery (Battery battery){
+        this.batteries.add(battery);
     }
 
-    public void turnOff() {
-        if (this.chargingStatus = true) {
-            this.chargingStatus = false;
-            System.out.println("Mein Name ist " + batteryName + " Kein Verbraucher angeschlossen");
-        }
+    public List<Battery> getBatteries() {
+        return batteries;
     }
 
+//    public void turnOn() {
+//        if (this.chargingStatus = false) {
+//            this.chargingStatus = true;
+//            this.batteryStatus = this.batteryStatus - ((this.batteryStatus / 100) * 5);
+//        } else {
+//            System.out.println("Mein Verbraucher ist bereits angeschlossen");
+//        }
+//    }
+//
+//    public void turnOff() {
+//        if (this.chargingStatus = true) {
+//            this.chargingStatus = false;
+//            System.out.println("Es ist kein Verbraucher angeschlossen");
+//        }
+//    }
 
     public boolean getChargingStatus() {
         return chargingStatus;
     }
+    public void setChargingStatus(boolean newChargingStatus) {
+        this.chargingStatus = newChargingStatus;
+    }
 
     public double getBatteryStatus() {
         return batteryStatus;
+    }
+
+    public void setBatteryStatus(double newBatteryStatus) {
+        this.batteryStatus = newBatteryStatus;
     }
 }
 
