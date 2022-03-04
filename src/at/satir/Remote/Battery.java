9f6b5797c58630a4b3b1baa6901e72c5a2587333
serkan <serkan.satirx61@gmail.com>
2022-03-04 12:07:07 +0100

    setter for BatteryStatus know in usage

--- src/at/satir/Remote/Battery.java
+++ src/at/satir/Remote/Battery.java
@@ -1,24 +1,12 @@
 package at.satir.Remote;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Battery {
 
     private double batteryStatus;
-    private List<Battery> batteries;
+
 
     public Battery(double batteryStatus) {
         this.batteryStatus = batteryStatus;
-        this.batteries = new ArrayList<>();
-    }
-
-    public void addBattery(Battery battery) {
-        this.batteries.add(battery);
-    }
-
-    public List<Battery> getBatteries() {
-        return batteries;
     }
 
     public double getBatteryStatus() {
@@ -26,16 +14,17 @@ public class Battery {
     }
 
     public void setBatteryStatus(double newBatteryStatus) {
-
         this.batteryStatus = newBatteryStatus;
     }
 
     public void givePower() {
         this.batteryStatus = this.batteryStatus - 5;
-        System.out.println("Verbraucher angeschlossen");
+        System.out.println("Verbraucher angeschlossen " + batteryStatus + "%");
     }
+
     public void loaderNotPluggedIn() {
-        System.out.println("Kein Verbraucher angeschlossen");
+        System.out.println("Kein Verbraucher angeschlossen " + batteryStatus + "%");
     }
+
 }
 
