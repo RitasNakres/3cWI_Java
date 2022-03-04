3adeedea7f28dc42f611457776ffc49fdb0e90a7
serkan <serkan.satirx61@gmail.com>
2022-03-04 12:05:54 +0100

    tried out getAllBatteryStatus method and it works

--- src/at/satir/Remote/Main.java
+++ src/at/satir/Remote/Main.java
@@ -5,21 +5,11 @@ public class Main {
 
         Battery b1 = new Battery(100);
         Battery b2 = new Battery(80);
-        Remote r1 = new Remote(b1,b2);
-
-
+        Remote r1 = new Remote(b1, b2);
 
         r1.turnOn();
-        System.out.println(b1.getBatteryStatus());
-        System.out.println(b2.getBatteryStatus());
-
         r1.turnOn();
-        System.out.println(b1.getBatteryStatus());
-        System.out.println(b2.getBatteryStatus());
-
         r1.getAllBatteryStatus();
-
-
 
 
 
