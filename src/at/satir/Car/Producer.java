c272dc6cac3df3797aa5ffa5943006a720086646
serkan <serkan.satirx61@gmail.com>
2022-03-16 09:24:22 +0100

    constructor set

--- src/at/satir/Car/Producer.java
+++ src/at/satir/Car/Producer.java
@@ -1,7 +1,16 @@
 package at.satir.Car;
 
 public class Producer {
+
     private String name;
     private String originCountry;
+    private double discount;
+
+    public Producer(String name, String originCountry, double discount) {
+        this.name = name;
+        this.originCountry = originCountry;
+        this.discount = discount;
+    }
+
 
 }
