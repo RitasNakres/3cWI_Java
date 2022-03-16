6d4c06576c4d7ae64f97457cb0e60cfc62493b24
serkan <serkan.satirx61@gmail.com>
2022-03-16 09:25:06 +0100

    constructor set

--- src/at/satir/Car/Engine.java
+++ src/at/satir/Car/Engine.java
@@ -1,6 +1,11 @@
 package at.satir.Car;
 
 public class Engine {
-    enum tankType {Diesel, Benzin}
 
+    enum tankType {Diesel, Benzin}
+    private double horsepower;
+
+    public Engine(double horsepower) {
+        this.horsepower = horsepower;
+    }
 }
