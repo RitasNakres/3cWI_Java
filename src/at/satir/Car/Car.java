3fd7727c203ad68390af3d23db9b014053ab7b54
serkan <serkan.satirx61@gmail.com>
2022-03-16 09:21:56 +0100

    Constructur is set

--- src/at/satir/Car/Car.java
+++ src/at/satir/Car/Car.java
@@ -1,9 +1,21 @@
 package at.satir.Car;
 
 public class Car {
+
+    private Producer producer;
+    private Engine engine;
     private String color;
     private double maximumSpeed;
     private double basePrice;
-    private Producer producer;
-    private Engine engine;
+
+
+    public Car(Producer producer, Engine engine, String color, double maximumSpeed, double basePrice) {
+        this.producer = producer;
+        this.engine = engine;
+        this.color = color;
+        this.maximumSpeed = maximumSpeed;
+        this.basePrice = basePrice;
+    }
+
+
 }
