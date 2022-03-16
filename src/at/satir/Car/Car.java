99411c48dc497f214092a190268abd98a3b0f858
serkan <serkan.satirx61@gmail.com>
2022-03-16 08:54:58 +0100

    instance variables for car set

--- src/at/satir/Car/Car.java
+++ src/at/satir/Car/Car.java
@@ -1,5 +1,9 @@
 package at.satir.Car;
 
 public class Car {
-
+    private String color;
+    private double maximumSpeed;
+    private double basePrice;
+    private Producer producer;
+    private Engine engine;
 }
