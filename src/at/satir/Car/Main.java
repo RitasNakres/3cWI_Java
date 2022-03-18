9eddeed86455409181f74e99d65a03cdd520d488
serkan <serkan.satirx61@gmail.com>
2022-03-18 11:47:25 +0100

    trying out the discount from the producer

--- src/at/satir/Car/Main.java
+++ src/at/satir/Car/Main.java
@@ -3,6 +3,15 @@ package at.satir.Car;
 public class Main {
     public static void main(String[] args) {
 
+        Engine e1 = new Engine(70,Engine.tankType.Gasoline);
+        Engine e2 = new Engine(120, Engine.tankType.Diesel);
+
+        Producer p1 = new Producer("VW","Germany",5);
+        Producer p2 = new Producer("Audi","Germany",10);
+
+        Car c1 = new Car(p1,e1,"red",180,12000);
+
+        c1.getSellingPrice();
 
 
     }
