781d5c57ecf77f73104d742f99978fdaf9e94efb
serkan <serkan.satirx61@gmail.com>
2022-03-25 12:08:55 +0100

    testing drive method again and it works now

--- src/at/satir/Car/Main.java
+++ src/at/satir/Car/Main.java
@@ -9,11 +9,12 @@ public class Main {
         Producer p1 = new Producer("VW", "Germany", 0.95);
         Producer p2 = new Producer("Audi", "Germany", 0.90);
 
-        Car c1 = new Car(p1, e1, "red", 160,10,44000,12500);
-        Car c2 = new Car(p2, e2, "black", 190, 10,45000,20000);
+        Car c1 = new Car(p1, e1, "red", 160, 10, 46000, 12500);
+        Car c2 = new Car(p2, e2, "black", 190, 10, 46000, 20000);
 
         c1.drive();
         c2.drive();
-
+        c1.newBaseComsumption();
+        c2.newBaseComsumption();
     }
 }
