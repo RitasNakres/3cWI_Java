b8c5263bf7d36f6caaae038ab3d2ca243e746e51
serkan <serkan.satirx61@gmail.com>
2022-03-30 09:35:24 +0200

    base calculator programmed

---
+++ src/at/satir/Calculator/Calculator.java
@@ -0,0 +1,19 @@
+package at.satir.Calculator;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
