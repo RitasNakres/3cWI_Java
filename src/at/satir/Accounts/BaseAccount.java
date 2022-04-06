9cff6052f01aa8c3947e8ee52ed62d0c0a69be93
serkan <serkan.satirx61@gmail.com>
2022-04-06 09:20:36 +0200

    method withdraw programmed

--- src/at/satir/Accounts/BaseAccount.java
+++ src/at/satir/Accounts/BaseAccount.java
@@ -7,8 +7,14 @@ public class BaseAccount {
         return balance;
     }
 
-    public void deposit (double amount) {
+    public void deposit(double amount) {
         System.out.println("Deposited: " + amount);
         this.balance += amount;
     }
+    public double withdraw (double amount) {
+        System.out.println("Withdrawn: " + amount);
+        this.balance -= amount;
+        return amount;
+    }
+
 }
