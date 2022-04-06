6b9f64179cd64ce1a97643e86a68654d3cf6a2c8
serkan <serkan.satirx61@gmail.com>
2022-04-06 09:31:38 +0200

    constructor set

--- src/at/satir/Accounts/CheckingAccount.java
+++ src/at/satir/Accounts/CheckingAccount.java
@@ -1,6 +1,12 @@
 package at.satir.Accounts;
 
 public class CheckingAccount extends BaseAccount {
+
     private int limit;
+
+    public CheckingAccount(int limit) {
+        this.limit = limit;
+    }
+
 
 }
