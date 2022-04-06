9f81386eb230498dd38a1e4aaec1ce70a46f7011
serkan <serkan.satirx61@gmail.com>
2022-04-06 08:13:23 +0200

    programmed sin method

--- src/at/satir/Calculator/ScientificCalculator.java
+++ src/at/satir/Calculator/ScientificCalculator.java
@@ -2,4 +2,8 @@ package at.satir.Calculator;
 
 public class ScientificCalculator extends Calculator {
 
+    public double sin(double Result) {
+        Result = Math.sin(Result);
+        return Result;
+    }
 }
