a219eabc54dcbf16554a7575692a66a045d1c070
serkan <serkan.satirx61@gmail.com>
2022-04-06 08:43:03 +0200

    programmed root method

--- src/at/satir/Calculator/RootCalculator.java
+++ src/at/satir/Calculator/RootCalculator.java
@@ -2,4 +2,8 @@ package at.satir.Calculator;
 
 public class RootCalculator extends Calculator {
 
+    public void root(double number) {
+        double result = Math.sqrt(number);
+        System.out.println(result);
+    }
 }
