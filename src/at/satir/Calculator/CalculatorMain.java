5594f3d1335927f2e25c505820074d520e7bb31f
serkan <serkan.satirx61@gmail.com>
2022-04-06 08:15:59 +0200

    testing method sin

--- src/at/satir/Calculator/CalculatorMain.java
+++ src/at/satir/Calculator/CalculatorMain.java
@@ -1,7 +1,14 @@
 package at.satir.Calculator;
 
+import javax.xml.transform.Result;
+
 public class CalculatorMain {
     public static void main(String[] args) {
 
+        Calculator c1 = new Calculator();
+        ScientificCalculator sc1 = new ScientificCalculator();
+        sc1.sin(85);
+        double result = c1.multiply(10, 7);
+        System.out.println(result);
     }
 }
