33bd0c32d885588d5dc78ab7a67006143e818e3a
serkan <serkan.satirx61@gmail.com>
2022-04-08 12:10:03 +0200

    tried out creditCosts Method

--- src/at/satir/Accounts/Main.java
+++ src/at/satir/Accounts/Main.java
@@ -2,16 +2,18 @@ package at.satir.Accounts;
 
 public class Main {
     public static void main(String[] args) {
-        CheckingAccount ca = new CheckingAccount(1500,2,4);
+        CheckingAccount ca = new CheckingAccount(1500, 2, 4);
         ca.deposit(5000);
         ca.withdraw(5000);
         System.out.println(ca.getBalance());
 
-        SavingAccount sa1 = new SavingAccount(3000,5,0.04);
+        SavingAccount sa1 = new SavingAccount(3000, 5, 0.04);
         sa1.accountInterest();
 
-        LittleCountryAccount lca1 = new LittleCountryAccount(3000,2);
+        LittleCountryAccount lca1 = new LittleCountryAccount(3000, 2);
         lca1.accountKeepingCosts(83.04);
 
+        CreditAccount ca1 = new CreditAccount(2000, 5);
+        ca1.creditCosts(-500, 0.023);
     }
 }
