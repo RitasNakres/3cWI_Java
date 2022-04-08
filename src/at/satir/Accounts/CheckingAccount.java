20f4595b1a3f9fd2616964974f34234ec8aa426a
serkan <serkan.satirx61@gmail.com>
2022-04-08 11:44:59 +0200

    method accountInterest adjusted for CheckingAccount

--- src/at/satir/Accounts/CheckingAccount.java
+++ src/at/satir/Accounts/CheckingAccount.java
@@ -2,11 +2,31 @@ package at.satir.Accounts;
 
 public class CheckingAccount extends BaseAccount {
 
+    private double membershipYears;
+    private double rateOfInterest;
     private int limit;
 
-    public CheckingAccount(int limit) {
-        this.limit = limit;
+    public CheckingAccount(double balance, double membershipYears, double rateOfInterest) {
+        super(balance);
+        this.membershipYears = membershipYears;
+        this.rateOfInterest = rateOfInterest;
     }
 
+    public void accountInterest(){
+        double accountInterest = (this.getBalance() * this.rateOfInterest) * this.membershipYears;
+        System.out.println("Du hast über die letzten " + this.membershipYears + " Jahre " + accountInterest + " Zinsen gezahlt.");
+        setBalance(getBalance()-accountInterest);
+    }
+
+    @Override
+    public double withdraw (double amount) {
+        if ((getBalance() - amount) > (limit * -1)) {
+            System.out.println("Withdrawn: " + amount + "€");
+            return super.withdraw(amount);
+        } else {
+            System.out.println("This action is not possible");
+            return 0;
+        }
+    }
 
 }
