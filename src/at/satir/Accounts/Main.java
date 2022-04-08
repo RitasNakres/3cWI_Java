f003efc851f1de414e1f6b52a811d442fb5b24e0
serkan <serkan.satirx61@gmail.com>
2022-04-08 11:59:27 +0200

    trying out accountKeepingCosts Method

--- src/at/satir/Accounts/Main.java
+++ src/at/satir/Accounts/Main.java
@@ -2,12 +2,16 @@ package at.satir.Accounts;
 
 public class Main {
     public static void main(String[] args) {
-        CheckingAccount ca = new CheckingAccount(1,1,1);
+        CheckingAccount ca = new CheckingAccount(1500,2,4);
         ca.deposit(5000);
         ca.withdraw(5000);
         System.out.println(ca.getBalance());
 
         SavingAccount sa1 = new SavingAccount(3000,5,0.04);
         sa1.accountInterest();
+
+        LittleCountryAccount lca1 = new LittleCountryAccount(3000,2);
+        lca1.accountKeepingCosts(83.04);
+
     }
 }
