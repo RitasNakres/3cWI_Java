3823843468a7f5ea031a7463c92d55fea0189f24
serkan <serkan.satirx61@gmail.com>
2022-04-08 10:55:14 +0200

    trying out checkingaccount

--- src/at/satir/Accounts/Main.java
+++ src/at/satir/Accounts/Main.java
@@ -2,6 +2,10 @@ package at.satir.Accounts;
 
 public class Main {
     public static void main(String[] args) {
+        CheckingAccount ca = new CheckingAccount(3000);
+        ca.deposit(5000);
+        ca.withdraw(5000);
+        System.out.println(ca.getBalance());
 
     }
 }
