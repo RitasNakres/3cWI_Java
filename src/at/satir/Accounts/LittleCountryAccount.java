42c536d9427841f64d40934806bc0df5647be143
serkan <serkan.satirx61@gmail.com>
2022-04-08 11:45:59 +0200

    instance variables membershipYears + accountManagement set

--- src/at/satir/Accounts/LittleCountryAccount.java
+++ src/at/satir/Accounts/LittleCountryAccount.java
@@ -1,4 +1,13 @@
 package at.satir.Accounts;
 
 public class LittleCountryAccount extends BaseAccount {
+
+    private double membershipYears;
+    private double accountManagement = 83.04;
+
+    public LittleCountryAccount(double balance) {
+        super(balance);
+    }
+
+
 }
