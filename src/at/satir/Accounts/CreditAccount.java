3e95fb5f948bdcde5f5ea4b8c269d41551e9cb00
serkan <serkan.satirx61@gmail.com>
2022-04-08 12:00:38 +0200

    instance variable + constructor set

--- src/at/satir/Accounts/CreditAccount.java
+++ src/at/satir/Accounts/CreditAccount.java
@@ -1,4 +1,13 @@
 package at.satir.Accounts;
 
 public class CreditAccount extends BaseAccount {
+
+    private double loanInterest = 0.023;
+    private double creditLimit = -200;
+    private double loanMonths;
+
+    public CreditAccount(double balance, double loanMonths) {
+        super(balance);
+        this.loanMonths = loanMonths;
+    }
 }
