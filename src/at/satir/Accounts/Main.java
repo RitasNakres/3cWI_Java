0d6a56bf4615edfd20beb63006d9fd7aed32785c
serkan <serkan.satirx61@gmail.com>
2022-04-08 11:42:31 +0200

    trying out accountInterest method

--- src/at/satir/Accounts/Main.java
+++ src/at/satir/Accounts/Main.java
@@ -2,10 +2,12 @@ package at.satir.Accounts;
 
 public class Main {
     public static void main(String[] args) {
-        CheckingAccount ca = new CheckingAccount(3000);
+        CheckingAccount ca = new CheckingAccount(1,1,1);
         ca.deposit(5000);
         ca.withdraw(5000);
         System.out.println(ca.getBalance());
 
+        SavingAccount sa1 = new SavingAccount(3000,5,0.04);
+        sa1.accountInterest();
     }
 }
