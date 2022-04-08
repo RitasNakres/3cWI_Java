4fdf433ee0edb0bb0c4d6b492f6f02acb3505406
serkan <serkan.satirx61@gmail.com>
2022-04-08 12:05:18 +0200

    new method creditCosts

--- src/at/satir/Accounts/CreditAccount.java
+++ src/at/satir/Accounts/CreditAccount.java
@@ -2,8 +2,6 @@ package at.satir.Accounts;
 
 public class CreditAccount extends BaseAccount {
 
-    private double loanInterest = 0.023;
-    private double creditLimit = -200;
     private double loanMonths;
 
     public CreditAccount(double balance, double loanMonths) {
@@ -11,12 +9,17 @@ public class CreditAccount extends BaseAccount {
         this.loanMonths = loanMonths;
     }
 
-    public double getLoanInterest() {
-        return loanInterest;
-    }
-
-    public double getCreditLimit() {
-        return creditLimit;
+    public void creditCosts(double creditLimit, double loanInterest) {
+        double creditCosts = 0;
+        if (this.getBalance() <= creditLimit) {
+            System.out.println("Du hast das Kontolimit überschritten.");
+        } else if (this.getBalance() >= 0) {
+            System.out.println("Du musst keine Kreditkosten abzahlen.");
+        } else {
+            creditCosts = (-this.getBalance() * loanInterest) * this.loanMonths;
+            System.out.println("Du musst " + creditCosts + "€ Kreditzinsen zahlen.");
+            setBalance(getBalance() - creditCosts);
+        }
     }
 
     public double getLoanMonths() {
