3a8fb4259f6eb7d1a60f991f7bdd77feba89ac3d
serkan <serkan.satirx61@gmail.com>
2022-04-08 11:00:47 +0200

    overrode withdraw method for savingaccount

--- src/at/satir/Accounts/SavingAccount.java
+++ src/at/satir/Accounts/SavingAccount.java
@@ -1,4 +1,14 @@
 package at.satir.Accounts;
 
 public class SavingAccount extends BaseAccount {
+
+    @Override
+    public double withdraw(double amount) {
+        if ((getBalance() - amount) > 0) {
+            return super.withdraw(amount);
+        } else {
+            System.out.println("This action is not possible");
+            return 0;
+        }
+    }
 }
