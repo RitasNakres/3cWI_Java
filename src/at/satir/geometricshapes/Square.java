a993e84975e701e5a4624e4c089310cd5be059bf
serkan <serkan.satirx61@gmail.com>
2022-04-20 09:23:23 +0200

    getArea method adjusted for square and constructor set

--- src/at/satir/geometricshapes/Square.java
+++ src/at/satir/geometricshapes/Square.java
@@ -1,4 +1,17 @@
 package at.satir.geometricshapes;
 
-public class Square {
+public class Square extends Figure {
+
+    private double lengthA;
+
+    public Square(String name, double lengthA) {
+        super(name);
+        this.lengthA = lengthA;
+    }
+
+    @Override
+    public double getArea() {
+        double newCalculatedArea = Math.pow(this.lengthA,2);
+        return newCalculatedArea;
+    }
 }
