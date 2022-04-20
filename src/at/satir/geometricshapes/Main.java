92f7445dc121783c0bb8fb1c1843045d2aba7306
serkan <serkan.satirx61@gmail.com>
2022-04-20 08:45:27 +0200

    trying out getArea

--- src/at/satir/geometricshapes/Main.java
+++ src/at/satir/geometricshapes/Main.java
@@ -3,5 +3,8 @@ package at.satir.geometricshapes;
 public class Main {
     public static void main(String[] args) {
 
+        Figure f1 = new Figure("Figure",100);
+        f1.getArea();
+        System.out.println(f1.getArea());
     }
 }
