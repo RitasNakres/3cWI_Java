1049c96166b1d8490e872db99b7aeca9bd7bc497
serkan <serkan.satirx61@gmail.com>
2022-04-20 09:08:38 +0200

    getArea method adjusted for rectangle and constructor set

--- src/at/satir/geometricshapes/Rectangle.java
+++ src/at/satir/geometricshapes/Rectangle.java
@@ -1,4 +1,19 @@
 package at.satir.geometricshapes;
 
-public class Rectangle {
+public class Rectangle extends Figure{
+
+    public double lengthA;
+    public double lengthB;
+
+    public Rectangle(String name, double lengthA, double lengthB) {
+        super(name);
+        this.lengthA = lengthA;
+        this.lengthB = lengthB;
+    }
+
+    @Override
+    public double getArea() {
+        double newCalculatedArea = this.lengthA * this.lengthB;
+        return newCalculatedArea;
+    }
 }
