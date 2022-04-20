bc847490c2b855302a0fc370cbea30842adb4774
serkan <serkan.satirx61@gmail.com>
2022-04-20 09:05:38 +0200

    getArea method adjusted for circle

--- src/at/satir/geometricshapes/Circle.java
+++ src/at/satir/geometricshapes/Circle.java
@@ -1,4 +1,18 @@
 package at.satir.geometricshapes;
 
-public class Circle {
+public class Circle extends Figure{
+
+    private double radius;
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        double pi = 3.14159265359;
+        double calculatedArea = 2 * this.radius * pi;
+        return calculatedArea;
+    }
 }
