f8da9bb4262939b319b21912969d35f196185c73
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:13:16 +0200

    Array List animals set and supplemented in constructor

--- src/at/satir/ISaHASa/Zoo.java
+++ src/at/satir/ISaHASa/Zoo.java
@@ -1,12 +1,17 @@
 package at.satir.ISaHASa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoo {
     private String street;
     private String name;
+    private List <Animal> animals;
 
     public Zoo(String street, String name) {
         this.street = street;
         this.name = name;
+        this.animals = new ArrayList<>();
     }
 
     // + -> public, - -> private
