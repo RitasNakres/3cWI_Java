2ac80a2f0cc909051d66f6675cbeeda94692f6ff
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:26:09 +0200

    trying out addAnimal and printAnimals method

---
+++ src/at/satir/ISaHASa/Main.java
@@ -0,0 +1,13 @@
+package at.satir.ISaHASa;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Animal(40,20);
+        Animal a2 = new Animal(50,30);
+
+        Zoo zoo = new Zoo("Kinderzoo","Elefantenstrasse");
+        zoo.addAnimal(a1);
+        zoo.addAnimal(a2);
+        zoo.printAnimals();
+    }
+}
