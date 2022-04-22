43a8a996d5288a43bbd25f452b2254f7ee953a32
serkan <serkan.satirx61@gmail.com>
2022-04-22 12:03:24 +0200

    instance variables capacity, constructor and ArrayList set

--- src/at/satir/Phone/SD.java
+++ src/at/satir/Phone/SD.java
@@ -1,4 +1,14 @@
 package at.satir.Phone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SD {
+    private int capacity;
+    private List<PhoneFile> files;
+
+    public SD(int capacity) {
+        this.capacity = capacity;
+        this.files = new ArrayList<>();
+    }
 }
