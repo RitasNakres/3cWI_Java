6545edbc4f592516e1ac8f437ff66ca90bd9d8a8
serkan <serkan.satirx61@gmail.com>
2022-04-22 12:01:02 +0200

    instance variables, getter and constructor set

---
+++ src/at/satir/Phone/PhoneFile.java
@@ -0,0 +1,25 @@
+package at.satir.Phone;
+
+public class PhoneFile {
+    private String extension;
+    private int size; //Megabyte
+    private String name;
+
+    public PhoneFile(String extension, int size, String name) {
+        this.extension = extension;
+        this.size = size;
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
