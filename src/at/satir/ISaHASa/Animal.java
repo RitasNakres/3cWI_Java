3b520d8ce6568c4d2365818676ee57f6f6c2c04f
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:04:59 +0200

    Constructor and Getter and Setter set for instance variable height and weight

--- src/at/satir/ISaHASa/Animal.java
+++ src/at/satir/ISaHASa/Animal.java
@@ -5,4 +5,24 @@ public class Animal {
     private int weight;
     private int height;
 
+    public Animal(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
