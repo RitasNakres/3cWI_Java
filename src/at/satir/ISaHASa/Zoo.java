f93ed5635b564f78734a874b1e2613d20c3785d0
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:07:14 +0200

    Getter Setter Constructor set for street and name

--- src/at/satir/ISaHASa/Zoo.java
+++ src/at/satir/ISaHASa/Zoo.java
@@ -3,4 +3,25 @@ package at.satir.ISaHASa;
 public class Zoo {
     private String street;
     private String name;
+
+    public Zoo(String street, String name) {
+        this.street = street;
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
