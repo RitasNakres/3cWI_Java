ef4e8fbd74298e1c5d821b13f0104f43123f3183
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:34:30 +0200

    dog has all attributes of animal but also own method bark

---
+++ src/at/satir/ISaHASa/Dog.java
@@ -0,0 +1,12 @@
+package at.satir.ISaHASa;
+
+public class Dog extends Animal {
+
+    public Dog(int weight, int height) {
+        super(weight, height);
+    }
+
+    public void bark() {
+        System.out.println("Wuff");
+    }
+}
