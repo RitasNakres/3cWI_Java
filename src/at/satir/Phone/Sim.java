1bf145dff7a3c3445f4bc1635463c91dab878169
serkan <serkan.satirx61@gmail.com>
2022-04-22 11:57:46 +0200

    getter and constructor set for id and number

--- src/at/satir/Phone/Sim.java
+++ src/at/satir/Phone/Sim.java
@@ -2,5 +2,18 @@ package at.satir.Phone;
 
 public class Sim {
     private int id;
+    private String number;
 
+    public Sim(int id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
