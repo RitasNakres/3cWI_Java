a9cd830c4a4051c8c5282de07b65d27cc8b164e0
serkan <serkan.satirx61@gmail.com>
2022-04-27 08:23:50 +0200

    method takePicture created for Camera

--- src/at/satir/Phone/Camera.java
+++ src/at/satir/Phone/Camera.java
@@ -4,4 +4,9 @@ public class Camera {
     private String resolution;
     private int fileNumber = 0;
 
+    public PhoneFile takePicture() {
+        PhoneFile file = new PhoneFile("jpeg",1000,"pic" + fileNumber);
+        fileNumber++;
+        return file;
+    }
 }
