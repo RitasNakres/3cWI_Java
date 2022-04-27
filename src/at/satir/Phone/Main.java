d201e4c5c7895f754a236ec536277fb47151c871
serkan <serkan.satirx61@gmail.com>
2022-04-27 08:50:39 +0200

    trying out takePicture method and it works properly

--- src/at/satir/Phone/Main.java
+++ src/at/satir/Phone/Main.java
@@ -3,11 +3,15 @@ package at.satir.Phone;
 public class Main {
     public static void main(String[] args) {
 
-        Sim sim = new Sim(1,"06606669451");
+        Sim sim = new Sim(1, "06606669451");
         SD sdCard = new SD(100000);
         Camera camera = new Camera();
 
-        Phone phone = new Phone(sdCard,sim,camera);
+        Phone phone = new Phone(sdCard, sim, camera);
         phone.doCall("06606438091");
+
+        phone.takePicture();
+        phone.takePicture();
+
     }
 }
