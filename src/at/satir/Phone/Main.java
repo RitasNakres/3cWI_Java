8305841b4ebb97b0482c299ef955919bf6e573a3
serkan <serkan.satirx61@gmail.com>
2022-04-27 08:25:47 +0200

    created a sim, sdCard, Camera and integrated them into Phone

--- src/at/satir/Phone/Main.java
+++ src/at/satir/Phone/Main.java
@@ -3,5 +3,11 @@ package at.satir.Phone;
 public class Main {
     public static void main(String[] args) {
 
+        Sim sim = new Sim(1,"06606669451");
+        SD sdCard = new SD(100000);
+        Camera camera = new Camera();
+
+        Phone phone = new Phone(sdCard,sim,camera);
+
     }
 }
