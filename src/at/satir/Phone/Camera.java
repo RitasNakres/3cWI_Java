bb343db4c091bf2082371e0c6a84c831821e1980
serkan <serkan.satirx61@gmail.com>
2022-04-29 11:43:15 +0200

    arraylist cameras set for Camera + new instance variable name

--- src/at/satir/Phone/Camera.java
+++ src/at/satir/Phone/Camera.java
@@ -1,12 +1,32 @@
 package at.satir.Phone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Camera {
-    private String resolution;
+    private int resolution;
     private int fileNumber = 0;
+    private List<Camera> cameras;
+    private String name;
+
+
+    public Camera(int resolution) {
+        this.resolution = resolution;
+        this.cameras = new ArrayList<>();
+    }
+
 
     public PhoneFile takePicture() {
-        PhoneFile file = new PhoneFile("jpeg",1000,"pic" + fileNumber);
+        PhoneFile file = new PhoneFile("jpeg", 50, "pic " + fileNumber);
         fileNumber++;
         return file;
+    }
+
+    public List<Camera> getCamera() {
+        return cameras;
+    }
+
+    public String getName(){
+        return name;
     }
 }
