9c85286f183b01e6a1d9ec1c6a9e4f4317c44e58
serkan <serkan.satirx61@gmail.com>
2022-05-20 12:04:34 +0200

    constructor, getter and setter set for SDCard

--- src/at/satir/Camera/SDCard.java
+++ src/at/satir/Camera/SDCard.java
@@ -3,5 +3,15 @@ package at.satir.Camera;
 public class SDCard {
     private int capacity;
 
+    public SDCard(int capacity) {
+        this.capacity = capacity;
+    }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
