cfc88fd933c401c6ca8742414d320abfce8113df
serkan <serkan.satirx61@gmail.com>
2022-05-20 12:02:28 +0200

    constructor, getter and setter set for Producer

--- src/at/satir/Camera/Producer.java
+++ src/at/satir/Camera/Producer.java
@@ -5,4 +5,24 @@ public class Producer {
     private String name;
     private String country;
 
+    public Producer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
