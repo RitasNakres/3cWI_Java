de20a8a771d60971bac7aa95afc64527f5b0590b
serkan <serkan.satirx61@gmail.com>
2022-05-20 11:59:05 +0200

    getter setter constructor set for Camera

--- src/at/satir/Camera/Camera.java
+++ src/at/satir/Camera/Camera.java
@@ -5,4 +5,34 @@ public class Camera {
     private int weight;
     private String color;
 
+
+    public Camera(int pixel, int weight, String color) {
+        this.pixel = pixel;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getPixel() {
+        return pixel;
+    }
+
+    public void setPixel(int pixel) {
+        this.pixel = pixel;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
