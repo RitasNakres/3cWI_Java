8c6b84da7b82b9ebc5adf905cb1b73cdceb314f3
serkan <serkan.satirx61@gmail.com>
2022-05-20 12:13:02 +0200

    constructor, getter and setter set for Image

--- src/at/satir/Camera/Image.java
+++ src/at/satir/Camera/Image.java
@@ -4,4 +4,25 @@ public class Image {
 
     private int size; //Gigabyte
     private String name;
+
+    public Image(int size, String name) {
+        this.size = size;
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
