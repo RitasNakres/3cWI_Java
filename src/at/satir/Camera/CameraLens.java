746a372a1a0635e64862be939218faf04bade5b9
serkan <serkan.satirx61@gmail.com>
2022-05-20 12:09:38 +0200

    getter and setter set for focalDistance instance variable

--- src/at/satir/Camera/CameraLens.java
+++ src/at/satir/Camera/CameraLens.java
@@ -4,5 +4,24 @@ public class CameraLens {
     private int focalDistance;
     private Producer producer;
 
+    public CameraLens(int focalDistance, Producer producer) {
+        this.focalDistance = focalDistance;
+        this.producer = producer;
+    }
 
+    public int getFocalDistance() {
+        return focalDistance;
+    }
+
+    public void setFocalDistance(int focalDistance) {
+        this.focalDistance = focalDistance;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
 }
